2e37610521c32be30f7d9cf5fcaa28ddb86e6599
ddtime123 <aa2797559967@gmail.com>
2021-10-27 20:25:26 +0800

    third commit : app get weather-data from web-api,and there is a thread running on background that keep updating weather-data per 20 seconds

---
+++ app/src/main/java/com/example/weatherapp/LongRunningService.java
@@ -0,0 +1,130 @@
+package com.example.weatherapp;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import org.json.JSONObject;
+
+import java.util.Date;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class LongRunningService extends Service {
+    private static final String TAG = "LongRunningService";
+
+    public static final int UPDATE_TEXT = 1;
+    private Handler handler = null;
+
+    public void sendMessage(Message msg){
+        Intent intent = new Intent("my-message");
+        intent.putExtra("my-msg",msg);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    public void getHandler(Handler handler){
+        this.handler = handler;
+    }
+
+    
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        // 开启线程来发起网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    // 要想发起请求，必须创建请求对象
+                    Request request = new Request.Builder()
+                            .url("https://tianqiapi.com/api?unescape=1&version=v6&appid=31225177&appsecret=eqIA7pd6")
+                            .build();
+                    // 调用 OkHttpClient 的 newCall() 方法来创建一个 Call 对象，
+                    // 并调用它的 execute 方法来发送请求并获取服务器返回的数据
+                    Response response = client.newCall(request).execute();
+                    // 获取 response 中的内容，其中 body 为响应正文
+                    String responseData = response.body().string();
+                    Log.d(TAG, responseData);
+
+                    // 接下来将响应正文解析成 JSON 格式数据
+                    Bundle bundle = parseJSONWithJSONObject(responseData);
+                    // 进行消息回调，将 JSON 文本传递会主线程
+                    Message msg = new Message();
+                    msg.what = UPDATE_TEXT;
+                    msg.setData(bundle);
+                    //handler.sendMessage(msg);
+                    sendMessage(msg);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        // 获取一个 AlarmManager 实例
+        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        // 设置一个定时任务
+        int temSec = 20 * 1000; // 20秒钟的毫秒数
+        long triggerAtTime = SystemClock.elapsedRealtime() + temSec; // 20秒钟
+        Intent i = new Intent(this, LongRunningService.class);
+        PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime, pi);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    // 解析 JSON
+    private Bundle parseJSONWithJSONObject(String jsonData) {
+        Bundle bundle = new Bundle();
+        try {
+            JSONObject jsonObject = new JSONObject(jsonData);
+            final String city = jsonObject.getString("city");
+            final String wea = jsonObject.getString("wea");
+            final String wea_img = jsonObject.getString("wea_img");
+            final String tem = jsonObject.getString("tem");
+            final String date = jsonObject.getString("date");
+            final String week = jsonObject.getString("week");
+            final String air = jsonObject.getString("air");
+
+            final String update_time = jsonObject.getString("update_time");
+
+            bundle.putString("city",city);
+            bundle.putString("wea",wea);
+            bundle.putString("wea_img",wea_img);
+            bundle.putString("tem",tem);
+            bundle.putString("update_time",update_time);
+            bundle.putString("date",date);
+            bundle.putString("week",week);
+            bundle.putString("air",air);
+
+            Log.d(TAG, "城市: "+city);
+            Log.d(TAG, "天气: "+wea);
+            Log.d(TAG, "天气图片: "+wea_img);
+            Log.d(TAG, "温度: "+tem);
+            Log.d(TAG, "小时: "+update_time);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            return bundle;
+        }
+    }
+}
--- app/src/main/java/com/example/weatherapp/WeatherActivity.java
+++ app/src/main/java/com/example/weatherapp/WeatherActivity.java
@@ -1,121 +1,184 @@
 package com.example.weatherapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Message;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
-public class WeatherActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "WeatherActivity";
-    TextView responseContent;
+    TextView weatherContent;
+    TextView dateContent;
+    Drawable weather_img;
 
-    public static final int UPDATE_TEXT = 1;
+    // 用于存储从相机拍摄回来的照片，用 ImageView 来存储
+    private ImageView picture;
+    // 用于指定照片存储位置的 Uri，这个 Uri 注册为 contentProvider
+    private Uri imageUri;
+    // 请求码，用于辨别返回的 Intent 应该对应的是哪一个结果，结果由请求方写
+    public static final int TAKE_PHOTO = 1;
 
-    // 消息回调函数
-    private Handler handler = new Handler(){
-        public void handleMessage(Message msg){
-            switch (msg.what){
-                case UPDATE_TEXT:
-                    // 可以在此处进行 UI 操作
-                    responseContent.setText("msg.city: "+msg.getData().getString("city"));
-                    break;
-                default:
-                    break;
-            }
+//    public static final int UPDATE_TEXT = 1;
+
+    // 处理从 Service 中获取的数据
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(messageReceiver, new IntentFilter("my-message"));
+    }
+
+    // Handling the received Intents for the "my-integer" event
+    private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Extract data included in the Intent
+            Message msg =(Message) intent.getParcelableExtra("my-msg");
+
+            // 首先设置天气显示
+            String city = msg.getData().getString("city");
+            String wea = msg.getData().getString("wea");
+            String update_time = msg.getData().getString("update_time");
+            String wea_img = msg.getData().getString("wea_img");
+
+            int wea_img_id = getResources().getIdentifier(wea_img , "mipmap" , getPackageName()) ;
+            weather_img = ContextCompat.getDrawable(context,wea_img_id);
+
+            String tem = msg.getData().getString("tem");
+            Log.d(TAG, "温度："+tem+" C\n"+city+"\n更新时间："+update_time+"\n");
+
+            weatherContent.setText("温度："+tem+" C\n"+city+"\n更新时间："+update_time+"\n");
+            weatherContent.setCompoundDrawablesWithIntrinsicBounds(null,weather_img,null,null);
+
+            // 接下来设置日期显示
+            String date = msg.getData().getString("date");
+            String week = msg.getData().getString("week");
+            String air = msg.getData().getString("air");
+            dateContent.setText("日期："+date+"\n"+week+"\n空气质量："+air+"\n");
         }
     };
+
+    // 获取从相机程序的请求码和返回码
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case TAKE_PHOTO:
+                if(resultCode == RESULT_OK){
+                    try{
+                        // 将拍摄的照片显示出来
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+             default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        // Unregister since the activity is not visible
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReceiver);
+        super.onPause();
+    }
+
+    // 消息回调函数
+//    private Handler handler = new Handler(){
+//        public void handleMessage(Message msg){
+//            switch (msg.what){
+//                case UPDATE_TEXT:
+//                    // 可以在此处进行 UI 操作
+//                    weatherContent.setText("msg.city: "+msg.getData().getString("city"));
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
+//    };
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "onClick: ");
+        switch (v.getId()){
+            case R.id.Contact:
+                break;
+            case R.id.Camera:{
+                // 创建 File 对象，用于存储拍摄的图片
+                File outputImage = new File(getExternalCacheDir() ,"output_image.jpg");
+                try{
+                    if(outputImage.exists()){
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+                if(Build.VERSION.SDK_INT >= 24){
+                    imageUri = FileProvider.getUriForFile(WeatherActivity.this,
+                            "com.example.cameraalbumtest.fileprovider",outputImage);
+                }else{
+                    imageUri = Uri.fromFile(outputImage);
+                }
+                // 启动相机程序
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+            }
+                break;
+            case R.id.Weixin: break;
+            case R.id.Call: break;
+            case R.id.Youku: break;
+            case R.id.Toutiao: break;
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        responseContent = (TextView) findViewById(R.id.WebContent);
-        sendRequestWithHttpURLConnection();
-    }
+        weatherContent = (TextView) findViewById(R.id.WebContent);
+        dateContent = (TextView) findViewById(R.id.DateContent);
+        Intent intent = new Intent(WeatherActivity.this,LongRunningService.class);
+        this.startService(intent);
 
-    public void sendRequestWithHttpURLConnection(){
-        // 开启线程来发起网络请求
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    OkHttpClient client = new OkHttpClient();
-                    // 要想发起请求，必须创建请求对象
-                    Request request = new Request.Builder()
-                            .url("https://www.tianqiapi.com/free/day?appid=31225177&appsecret=eqIA7pd6&unescape=1")
-                            .build();
-                    // 调用 OkHttpClient 的 newCall() 方法来创建一个 Call 对象，
-                    // 并调用它的 execute 方法来发送请求并获取服务器返回的数据
-                    Response response = client.newCall(request).execute();
-                    // 获取 response 中的内容，其中 body 为响应正文
-                    String responseData = response.body().string();
-                    Log.d(TAG, responseData);
+        ImageButton btn_contact = (ImageButton) findViewById(R.id.Contact);
+        ImageButton btn_camera = (ImageButton) findViewById(R.id.Camera);
+        ImageButton btn_weixin = (ImageButton) findViewById(R.id.Weixin);
+        ImageButton btn_call = (ImageButton) findViewById(R.id.Call);
+        ImageButton btn_youku = (ImageButton) findViewById(R.id.Youku);
+        ImageButton btn_toutiao = (ImageButton) findViewById(R.id.Toutiao);
 
-                    // 接下来将响应正文解析成 JSON 格式数据
-                    Bundle bundle = parseJSONWithJSONObject(responseData);
-                    // 进行消息回调，将 JSON 文本传递会主线程
-                    Message msg = new Message();
-                    msg.what = UPDATE_TEXT;
-                    msg.setData(bundle);
-                    handler.sendMessage(msg);
-
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
-
-    private void showResponse(final String response){
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                // 进行 UI 操作，将结果显示在界面上
-                responseContent.setText(response);
-            }
-        });
-    }
-
-    private Bundle parseJSONWithJSONObject(String jsonData) {
-        Bundle bundle = new Bundle();
-        try {
-            JSONObject jsonObject = new JSONObject(jsonData);
-            final String city = jsonObject.getString("city");
-            final String wea = jsonObject.getString("wea");
-            final String wea_img = jsonObject.getString("wea_img");
-            final String tem = jsonObject.getString("tem");
-
-            final String update_time = jsonObject.getString("update_time");
-
-            bundle.putString("city",city);
-            bundle.putString("wea",wea);
-            bundle.putString("wea_img",wea_img);
-            bundle.putString("tem",tem);
-            bundle.putString("update_time",update_time);
-
-            Log.d(TAG, "城市: "+city);
-            Log.d(TAG, "天气: "+wea);
-            Log.d(TAG, "天气图片: "+wea_img);
-            Log.d(TAG, "温度: "+tem);
-            Log.d(TAG, "小时: "+update_time);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally {
-            return bundle;
-        }
+        btn_camera.setOnClickListener(this);
     }
 
 
